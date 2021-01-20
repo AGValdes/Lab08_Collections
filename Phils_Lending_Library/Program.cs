77f4a24bba41354d28f84f1bbc6b035aa5a8e93b
ameilia valdes <valdes.ameilia@gmail.com>
2021-01-20 14:11:33 -0800

    initial commit

--- Phils_Lending_Library/Program.cs
+++ Phils_Lending_Library/Program.cs
@@ -6,7 +6,7 @@ namespace Phils_Lending_Library
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
